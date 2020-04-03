47b0e6aff2af6a6db3b66a7f3fdbc152f24466a3
owensnick <owensnick@gmail.com>
2020-04-03 11:06:32 +0100

    Files generated by PkgTemplates

---
+++ src/GenomeFragments.jl
@@ -0,0 +1,5 @@
+module GenomeFragments
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using GenomeFragments
+using Test
+
+@testset "GenomeFragments.jl" begin
+    # Write your own tests here.
+end
